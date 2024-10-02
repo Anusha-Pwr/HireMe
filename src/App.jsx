16cc6df79b9d047b73bb296b234fc0b441950e49
Anusha-Pwr <anusha00466@gmail.com>
2024-10-02 16:12:10 +0530

    Define all the routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,56 @@
-import { useState } from 'react'
 import './App.css';
 import {Button} from "./components/ui/button";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AppLayout from './layout/app-layout';
+import LandingPage from "./pages/LandingPage";
+import OnBoarding from './pages/OnBoarding';
+import JobListing from "./pages/JobListing";
+import Job from "./pages/Job";
+import PostJob from "./pages/PostJob";
+import SavedJobs from "./pages/SavedJobs";
+import MyJobs from "./pages/MyJobs";
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <LandingPage />
+        },
+        {
+          path: "/onboarding",
+          element: <OnBoarding />
+        },
+        {
+          path: "/jobs",
+          element: <JobListing />
+        },
+        {
+          path: "/job/:id",
+          element: <Job />
+        },
+        {
+          path: "/post-job",
+          element: <PostJob />
+        },
+        {
+          path: "/saved-jobs",
+          element: <SavedJobs />
+        },
+        {
+          path: "/my-jobs",
+          element: <MyJobs />
+        }
+      ]
+    }
+  ])
+
   return (
     <>
-      <Button>Hello</Button>
+      <RouterProvider router={router} />
     </>
   )
 }
