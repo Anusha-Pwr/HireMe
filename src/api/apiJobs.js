7d5b43b847ffd058065a15cc3cfde7832051d7ac
Anusha-Pwr <anusha00466@gmail.com>
2024-10-06 17:10:07 +0530

    Add saveJob function in apiJobs.js

--- src/api/apiJobs.js
+++ src/api/apiJobs.js
@@ -1,29 +1,60 @@
 import supabaseClient from "../utils/supabase";
 
+export async function getJobs(token, { location, company_id, searchQuery }) {
+  const supabase = await supabaseClient(token);
 
-export async function getJobs(token, {location, company_id, searchQuery}) {
-    const supabase = await supabaseClient(token);
+  let query = await supabase
+    .from("jobs")
+    .select("*, company: companies(name, logo_url), saved: saved_jobs(id)");
 
-    let query = await supabase.from("jobs").select("*, company: companies(name, logo_url), saved: saved_jobs(id)");
+  if (location) {
+    query = query.eq("location", location);
+  }
 
-    if(location) {
-        query = query.eq("location", location);
-    }
+  if (company_id) {
+    query = query.eq("company_id", company_id);
+  }
 
-    if(company_id) {
-        query = query.eq("company_id", company_id);
-    }
+  if (searchQuery) {
+    query = query.ilike("title", `%${searchQuery}%`); // case insensitive search in the 'title' column
+  }
 
-    if(searchQuery) {
-        query = query.ilike("title", `%${searchQuery}%`); // case insensitive search in the 'title' column
-    }
+  const { data, error } = query;
 
-    const {data, error} = query;
+  if (error) {
+    console.log("Error fetching jobs:", error);
+    return null;
+  }
 
-    if(error) {
-        console.log("Error fetching jobs:", error);
-        return null;
+  return data;
+}
+
+export async function saveJob(token, alreadySaved, saveJobData) {
+  const supabase = await supabaseClient(token);
+
+  if (alreadySaved) {
+    const { data, error: deleteError } = await supabase
+      .from("saved_jobs")
+      .delete()
+      .eq("job_id", saveJobData.job_id);
+
+    if (deleteError) {
+      console.error("Error removing saved job: ", deleteError);
+      return null;
     }
 
     return data;
+  } else {
+    const { data, error: insertError } = await supabase
+      .from("saved_jobs")
+      .insert([saveJobData])
+      .select();
+
+    if (insertError) {
+      console.error("Error inserting saved job: ", insertError);
+      return null;
+    }
+
+    return data;
+  }
 }
