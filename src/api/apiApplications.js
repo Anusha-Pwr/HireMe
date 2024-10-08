2f3cb13923fde3072142be71d312c4f4c94fd618
Anusha-Pwr <anusha00466@gmail.com>
2024-10-08 13:30:23 +0530

    Add applyToJob function in apiApplications.js to insert an application in supabase

---
+++ src/api/apiApplications.js
@@ -0,0 +1,29 @@
+import supabaseClient, {supabaseUrl} from "../utils/supabase";
+
+export async function applyToJob(token, _, candidateData) {
+    const supabase = await supabaseClient(token);
+
+    const random = Math.floor(Math.random()*90000);
+    const fileName = `resume-${random}-${candidateData.candidate_id}`
+
+    const {error: storageError} = await supabase.storage.from("resumes").upload(fileName, candidateData.resume);
+
+    if(storageError) {
+        console.log("Error uploading resume: ", storageError);
+        return null;
+    }
+
+    const resumeUrl = `${supabaseUrl}/storage/v1/object/public/resumes/${fileName}`;
+
+    const {data, error} = await supabase.from("applications").insert[{
+        ...candidateData,
+        resumeUrl
+    }].select();
+
+    if(error) {
+        console.log("Error submitting application: ", error);
+        return null;
+    }
+
+    return data;
+}
