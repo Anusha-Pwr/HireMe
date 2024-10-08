b88c834064d938e495fcb60c2ccf3d048f943f39
Anusha-Pwr <anusha00466@gmail.com>
2024-10-08 16:46:45 +0530

    Add form validation in ApplyJobDrawer.jsx and call applyToJob function to submit the application data

--- src/api/apiApplications.js
+++ src/api/apiApplications.js
@@ -1,29 +1,37 @@
-import supabaseClient, {supabaseUrl} from "../utils/supabase";
+import supabaseClient, { supabaseUrl } from "../utils/supabase";
 
 export async function applyToJob(token, _, candidateData) {
-    const supabase = await supabaseClient(token);
+  const supabase = await supabaseClient(token);
 
-    const random = Math.floor(Math.random()*90000);
-    const fileName = `resume-${random}-${candidateData.candidate_id}`
+  const random = Math.floor(Math.random() * 90000);
+  const fileName = `resume-${random}-${candidateData.candidate_id}`;
 
-    const {error: storageError} = await supabase.storage.from("resumes").upload(fileName, candidateData.resume);
+  const { error: storageError } = await supabase.storage
+    .from("resumes")
+    .upload(fileName, candidateData.resume);
 
-    if(storageError) {
-        console.log("Error uploading resume: ", storageError);
-        return null;
-    }
+  if (storageError) {
+    console.log("error here");
+    console.log("Error uploading resume: ", storageError);
+    return null;
+  }
 
-    const resumeUrl = `${supabaseUrl}/storage/v1/object/public/resumes/${fileName}`;
+  const resumeUrl = `${supabaseUrl}/storage/v1/object/public/resumes/${fileName}`;
 
-    const {data, error} = await supabase.from("applications").insert[{
+  const { data, error } = await supabase
+    .from("applications")
+    .insert([
+      {
         ...candidateData,
-        resumeUrl
-    }].select();
+        resume: resumeUrl,
+      },
+    ])
+    .select();
 
-    if(error) {
-        console.log("Error submitting application: ", error);
-        return null;
-    }
+  if (error) {
+    console.log("Error submitting application: ", error);
+    return null;
+  }
 
-    return data;
+  return data;
 }
