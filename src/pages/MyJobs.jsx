6a3d566a9404ce54a7f02b940e365f0af483b509
Anusha-Pwr <anusha00466@gmail.com>
2024-10-09 17:30:04 +0530

    Add MyJobs.jsx and render either CreatedJobs.jsx or CreatedApplications.jsx based on user's role

--- src/pages/MyJobs.jsx
+++ src/pages/MyJobs.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useUser } from "@clerk/clerk-react";
+import React from "react";
+import { BarLoader } from "react-spinners";
+import CreatedApplications from "../components/CreatedApplications";
+import CreatedJobs from "../components/CreatedJobs";
 
 const MyJobs = () => {
-  return (
-    <div>MyJobs</div>
-  )
-}
+  const { user, isLoaded } = useUser();
 
-export default MyJobs
+  if (!isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="#7b68ee" />;
+  }
+
+  return (
+    <div>
+      <h1 className="gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8">
+        {user?.unsafeMetadata?.role === "recruiter"
+          ? "My Jobs"
+          : "My Applications"}
+      </h1>
+
+      {user?.unsafeMetadata?.role === "recruiter" ? (
+        <CreatedJobs />
+      ) : (
+        <CreatedApplications />
+      )}
+    </div>
+  );
+};
+
+export default MyJobs;
